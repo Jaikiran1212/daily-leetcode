8b138cfee2f273206ad0aa739dc6dcc3c6111ff0
Jaikiran J <jaikiranjp@gmail.com>
2026-08-14 22:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (45.83%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+int** mergeArrays(int** nums1, int nums1Size, int* nums1ColSize, 
+                  int** nums2, int nums2Size, int* nums2ColSize, 
+                  int* returnSize, int** returnColumnSizes) {
+    int i = 0, j = 0, k = 0;
+    int maxPossibleSize = nums1Size + nums2Size;
+    
+    int** result = (int**)malloc(sizeof(int*) * maxPossibleSize);
+    *returnColumnSizes = (int*)malloc(sizeof(int) * maxPossibleSize);
+
+    while (i < nums1Size && j < nums2Size) {
+        result[k] = (int*)malloc(sizeof(int) * 2);
+        (*returnColumnSizes)[k] = 2;
+
+        if (nums1[i][0] == nums2[j][0]) {
+            result[k][0] = nums1[i][0];
+            result[k][1] = nums1[i][1] + nums2[j][1];
+            i++;
+            j++;
+        } else if (nums1[i][0] < nums2[j][0]) {
+            result[k][0] = nums1[i][0];
+            result[k][1] = nums1[i][1];
+            i++;
+        } else {
+            result[k][0] = nums2[j][0];
+            result[k][1] = nums2[j][1];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < nums1Size) {
+        result[k] = (int*)malloc(sizeof(int) * 2);
+        (*returnColumnSizes)[k] = 2;
+        result[k][0] = nums1[i][0];
+        result[k][1] = nums1[i][1];
+        i++;
+        k++;
+    }
+
+    while (j < nums2Size) {
+        result[k] = (int*)malloc(sizeof(int) * 2);
+        (*returnColumnSizes)[k] = 2;
+        result[k][0] = nums2[j][0];
+        result[k][1] = nums2[j][1];
+        j++;
+        k++;
+    }
+
+    *returnSize = k;
+    return result;
+}
